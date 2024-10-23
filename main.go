ff1ccc03c2c09a8e9785557e63b10214afc9682f
Zivision <zcastro703@gmail.com>
2024-10-23 10:08:34 -0500

    Simple HTTP Server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got Request")
+	io.WriteString(w, "This is the root page. Welcome!\n")
+}
 
 func main() {
-	fmt.Println("Hello World!");
+	const PORT string = ":8080"
+	// Routes
+	http.HandleFunc("/", getRoot)
+
+	// Start Server
+	fmt.Println("Server running on localhost" + PORT);
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
